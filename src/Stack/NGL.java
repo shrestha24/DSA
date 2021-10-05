625788653f1969adbf3bac18972a6a01078905ba
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2021-10-05 20:26:08 +0530

    Nearest Greatest Left

---
+++ src/Stack/NGL.java
@@ -0,0 +1,42 @@
+package Stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class NGL {
+    //Nearest Greatest Left
+    static List<Integer> ngl(int ar[]){
+        Stack<Integer> st = new Stack<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
+
+        for (int i = 0; i < ar.length; i++) {
+            if (st.isEmpty()){
+                list.add(-1);
+            }
+            else {
+                while (!st.isEmpty() && ar[i] >= st.peek()){
+                    st.pop(); }
+
+                    if (st.isEmpty()){
+                        list.add(-1);
+                    }
+                    else {
+                        list.add(st.peek());
+                    }
+            }
+            st.push(ar[i]);
+        }
+
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int ar[] = {12, 34, 32, 43, 23, 24};
+        System.out.println(ngl(ar).toString());
+    }
+
+}
+              //12 34 32 43 23 24
+              //-1 -1 34 -1 43 43
