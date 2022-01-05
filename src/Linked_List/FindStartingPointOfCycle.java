b205146a6a6798071e8d2c3600d092420bde61ff
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-01-05 20:19:44 +0530

    Find Starting Point Of Cycle

---
+++ src/Linked_List/FindStartingPointOfCycle.java
@@ -0,0 +1,25 @@
+package Linked_List;
+
+public class FindStartingPointOfCycle {
+        public ListNode detectCycle(ListNode head){
+            if(head == null || head.next == null)
+                return null;
+
+            ListNode slow = head;
+            ListNode fast = head;
+            ListNode entry = head;
+
+            while (fast.next != null && fast.next.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast){
+                    while (slow != entry) {
+                        slow = slow.next;
+                        entry = entry.next;
+                    }
+                    return entry;
+                }
+            }
+            return null;
+    }
+}
