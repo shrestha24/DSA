bed7b62e95d2ee96dafba6a67502fbafb8e1dfb3
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-01-18 14:27:42 +0530

    Check two trees are identical or not

---
+++ src/Tree/CheckTwoTreesAreIdentical.java
@@ -0,0 +1,12 @@
+package Tree;
+
+public class CheckTwoTreesAreIdentical {
+    public boolean isSameTree (TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return (p == q);
+        }
+
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
+}
