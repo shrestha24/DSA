36b2e3d3052466f6aee35d2f8996ff84ef596e60
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-02-08 00:33:54 +0530

    Shortest Path Binary Matrix.java

---
+++ src/graph/ShortestPathBinaryMatrix.java
@@ -0,0 +1,23 @@
+/*
+package graph;
+
+public class ShortestPathBinaryMatrix {
+    static class pair{
+        int r;
+        int c;
+
+        public pair(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+
+        public int shortestPathBinaryMatrix(int [][] grid) {
+            int n = grid.length;
+            if (grid[0][0] != 0) return -1;
+            if (n == 1)
+                return 1;
+
+        }
+    }
+}
+*/
