fcc29e7f0d385c59b8ca67220a7e2a54d581ee63
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-02-08 13:15:08 +0530

    Insert_Search_StartsWith in using Trie

---
+++ src/Trie/Insert_Search_StartsWith.java
@@ -0,0 +1,68 @@
+package Trie;
+
+
+     class Node{
+        Node links[] = new Node[26];
+        boolean flag = false;
+
+        public Node(){}
+
+        boolean containsKey(char ch){
+            return (links[ch - 'a'] != null);
+        }
+
+        Node get(char ch){
+            return links[ch - 'a'];
+        }
+
+        void put(char ch, Node node){
+            links[ch - 'a'] = node;
+        }
+        void setEnd() {
+            flag = true;
+        }
+        boolean isEnd(){
+            return flag;
+        }
+    };
+    class Trie{
+        private static Node root;
+
+        Trie() {
+            root = new Node();}
+
+        public static void insert(String word){
+            Node node = root;
+            for (int i = 0; i < word.length(); i++) {
+                if (!node.containsKey(word.charAt(i))) {
+                    node.put(word.charAt(i), new Node());
+                }
+                node = node.get(word.charAt(i));
+            }
+            node.setEnd();
+        }
+        public static boolean search(String word) {
+            Node node = root;
+            for (int i = 0; i < word.length(); i++) {
+                if (!node.containsKey(word.charAt(i))){
+                    return false;
+                }
+                node = node.get(word.charAt(i));
+            }
+            if (node.isEnd()){
+                return true;
+            }
+            return false;
+        }
+
+        public static boolean startWith(String prefix){
+            Node node = root;
+            for (int i = 0; i < prefix.length(); i++) {
+                if (!node.containsKey(prefix.charAt(i))){
+                    return false;
+                }
+                node = node.get(prefix.charAt(i));
+            }
+            return true;
+        }
+    }
