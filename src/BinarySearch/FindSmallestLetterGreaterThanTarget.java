a2ca155402c2e5a6c6eb07d219a5d7bad6b8a6d0
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-02-17 16:49:25 +0530

    Find Smallest Letter Greater Than Target

---
+++ src/BinarySearch/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+
+public class SearchInANearlySortedArray {
+    int search(int ar[], int target){
+        int start = 0;
+
+        int end = ar.length - 1;
+
+        while(start<=end){
+            int mid = start + (end - start) / 2;
+
+            if(ar[mid] ==  target){
+                return mid;
+            }
+            else if(mid+1 <= ar.length -1 && ar[mid+1]>= target){
+                return mid+1;
+            }
+            else if(mid -1 >= start && ar[mid-1] <= target){
+                return mid-1;
+            }
+            else if (ar[mid] < target){
+               end = mid -2;
+            }
+            else if(ar[mid] > target) {
+                start = mid + 2;
+            }
+
+        }
+        return -1;
+    }
+}
