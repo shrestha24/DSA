3b9920bb0f39630b67034e5592a8c5f6461442b2
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-03-05 09:19:27 +0530

    find Disapperance in Array

---
+++ src/Array/findDisapperance.java
@@ -0,0 +1,16 @@
+/*
+package Array;
+
+import java.util.List;
+
+public class findDisapperance {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+    }
+
+    public void cyclic(int ar[]){
+
+    }
+
+}
+*/
