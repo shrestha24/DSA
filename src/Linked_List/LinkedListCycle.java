0f238f8362f144b53b1ba9fb584d2addd359fbdc
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-03-18 14:34:50 +0530

    Linked List CYcle

---
+++ src/Linked_List/LinkedListCycle.java
@@ -0,0 +1,21 @@
+package Linked_List;
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+          if(head == null || head.next == null)
+              return false;
+
+          ListNode fast = head;
+          ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow)
+                return true;
+        }
+        return false;
+
+    }
+}
