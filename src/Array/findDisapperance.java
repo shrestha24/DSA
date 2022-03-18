1b026d01a4a4f3dc85e93edee84812f93554533c
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-03-18 14:33:27 +0530

    find Disappearance

--- src/Array/findDisapperance.java
+++ src/Array/findDisapperance.java
@@ -1,16 +1,36 @@
-/*
 package Array;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class findDisapperance {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        int n = nums.length;
 
+        while (i < n) {
+            int correctIndex = nums[i] - 1;
+            if (nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else i++;
+        }
+
+
+        List<Integer> ans = new ArrayList();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1) {
+                ans.add(index + 1);
+            }
+        }
+
+       return ans;
     }
 
-    public void cyclic(int ar[]){
+    private void swap(int[] ar, int i, int correctIndex) {
 
+        int temp = ar[i];
+        ar[i] = ar[correctIndex];
+        ar[correctIndex] = temp;
     }
 
 }
-*/
