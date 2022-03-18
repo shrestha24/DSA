34cf561814fbc4d58ea77321a9afba0d052f4d67
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-03-18 14:30:17 +0530

    Binary Search

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -22,16 +22,34 @@ public class BinarySearch {
     }
 
 
-
     //count of an element in an sorted array
 
 
-
     public static void main(String args[]) {
-        int ar[] = {10, 20, 30, 40, 50, 60, 70};
+        int ar[] = {10, 20, 30, 40, 50, 60};
         int x = 23;
         BinarySearch obj = new BinarySearch();
         System.out.println(obj.getBinary(ar, x));
+    }
+
+    public int getBinary1(int arr[], int val) {
+
+        int low = 0;
+        int high = arr.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == val) {
+                return mid;
+
+            } else if (arr[mid] < val)
+                high = mid - 1;
+
+            else
+                low = mid + 1;
+        }
+
+        return arr[low] == val ? low : -1;
     }
 
 }
