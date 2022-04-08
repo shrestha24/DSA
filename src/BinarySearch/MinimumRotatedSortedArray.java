7fe957766b2c4c3db43d2d3950c92b353ae57db8
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2022-04-08 10:30:43 +0530

    Minimum rotated sorted array

---
+++ src/BinarySearch/MinimumRotatedSortedArray.java
@@ -0,0 +1,18 @@
+package BinarySearch;
+
+public class MinimumRotatedSortedArray {
+    public int findMin(int[] nums) {
+
+         int start = 0;
+            int end = nums.length - 1;
+
+            while(start < end) {
+                int mid = start + (end - start) / 2;
+                if(nums[mid] > nums[end])
+                    start = mid + 1;
+                if(nums[mid]< nums[end])
+                end = mid;
+        }
+        return nums[start];
+    }
+}
